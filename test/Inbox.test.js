64f77ba57123c1754a2522969383aa79ad48ba23
karczu <karczupl@gmail.com>
2021-04-11 20:04:37 +0200

    test deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,22 +2,26 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //konstruktor
 const web3 = new Web3(ganache.provider()); //instancja
+const { interface, bytecode } = require('../compile');
 
 //w jednej aplikacji mozna korzystać z kilku instancji web3 
 //każda instancja ma providera- konfigurację do komunikacji z daną siecią Eth
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     //get a list of all account
     accounts = await web3.eth.getAccounts();
 
     //use one of those accounts to create contract
-
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox',() => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 });
